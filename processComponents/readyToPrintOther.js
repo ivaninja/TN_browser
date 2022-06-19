3f2179d9f72aa5c8fa1d6b4a0185ca281eac5bb8
Yuliya Khazran <ykhazran@gmail.com>
2022-06-19 11:19:15 +0200

    version 30 - addition printer settings

--- processComponents/readyToPrintOther.js
+++ processComponents/readyToPrintOther.js
@@ -1,17 +1,7 @@
 module.exports = function () {
+    console.log(this.settings.ticketPrinter);
     this.printWin.webContents.print({
-        silent: true,
-        margins: {
-            marginType: 'custom',
-            top: 0,
-            bottom: 0,
-            left: 0,
-            right: 20,
-        },
         deviceName:this.settings.ticketPrinter,
-        dpi: {
-            horizontal: 600,
-            vertical: 600,
-        },
+        silent: true
     });
 };
